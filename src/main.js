0b2a5936864813164957afcff7a4564003c56adf
NADA <senoagung27@gmail.com>
2026-02-18 16:20:26 +0700

    Feat: Integrate Vercel Analytics

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 
+import { inject } from '@vercel/analytics'
+
 const pinia = createPinia()
 const app = createApp(App)
+
+inject() // Initialize Vercel Analytics
 
 app.use(pinia)
 app.mount('#app')
